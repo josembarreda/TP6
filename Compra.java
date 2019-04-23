cacea61d88395daddb959e2c7cf1beccaefd78c1
josembarreda <josemanuelbarreda11@gmail.com>
2019-04-23 22:10:05 +0200

    Second commit

--- Compra.java
+++ Compra.java
@@ -1,15 +1,18 @@
 import java.io.Serializable;
 import java.lang.Double;
+import java.util.Date;
 
 public class Compra implements Serializable{
   private Person per;
   private Producto ap;
   private Double cant;
+  private Date date;
   public Compra() {
     // constructor por defecto
     per = new Person();
     ap = new Producto();
     cant = 0.0;
+    date= new Date();
   }
   public void setPer(String name) {
     per.setName(name);
@@ -20,6 +23,9 @@ public class Compra implements Serializable{
   public void setCant(Double cant) {
     this.cant = cant;
   }
+  public void setFecha(Date date){
+    this.date=date;
+  }
   public Person getPer() {
     return per;
   }
@@ -29,5 +35,8 @@ public class Compra implements Serializable{
 
   public Double getCant() {
     return cant;
+  }
+  public Date getFecha(){
+    return date;
   }
 }
